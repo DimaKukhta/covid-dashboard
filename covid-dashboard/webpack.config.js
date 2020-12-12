fe5ad25c465098ecc196584524a822829d102fd3
Dima Kukhta <dima.kukhta.2001@mail.ru>
2020-12-12 14:47:38 +0200

    feat: add webpack

---
+++ covid-dashboard/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js', 
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 4200
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [ 
+            {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+            },
+            {
+            test: /.(ogg|mp3|wav|mpe?g)$/,
+            use: [
+                {
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/./assets/[hash][query].[ext]',
+                    },
+                },
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
