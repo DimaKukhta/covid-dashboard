1fa4e515117138db10941dfdee4e24614ded4948
Sakharuk Viktar <natgeogr93@gmail.com>
2020-12-20 01:54:07 +0300

    feat: toggle buttons. refactor control-panel make up

--- covid-dashboard/src/index.js
+++ covid-dashboard/src/index.js
@@ -1,1 +1,5 @@
 import './style/style.css';
+import controlPanel from './js/controlPanel/controlPanel';
+
+controlPanel.fillControlPanelData();
+controlPanel.toggleLogic();
---
+++ covid-dashboard/src/js/controlPanel/controlPanel.js
@@ -0,0 +1,41 @@
+import { getCountriesAndGlobalInfo } from '../getApiData';
+
+function fillControlPanelData() {
+  const totalAmount = document.querySelector('#totalAmount');
+  const updatedDate = document.querySelector('#updatedDate');
+
+  getCountriesAndGlobalInfo().then(({ Global }) => {
+    const addZeroes = (n) => ((n < 10) ? `0${n}` : n);
+
+    const date = new Date(Global.updated);
+    const day = addZeroes(date.getDate());
+    const month = addZeroes(date.getMonth());
+    const year = date.getFullYear();
+    const hours = addZeroes(date.getHours());
+    const minutes = addZeroes(date.getMinutes());
+
+    updatedDate.textContent = `${day}.${month}.${year} | ${hours}:${minutes}`;
+
+    totalAmount.textContent = Global.cases;
+  });
+}
+
+function toggleLogic() {
+  const filters = document.querySelector('.button-filters');
+
+  function toggleFilter({ target }) {
+    const isTargetActive = target.classList.contains('button-active');
+    if (isTargetActive) return;
+    if (target !== this) {
+      const activeBtn = filters.querySelector('.button-active');
+      activeBtn.classList.remove('button-active');
+      target.classList.add('button-active');
+    }
+  }
+  filters.addEventListener('click', toggleFilter);
+}
+
+export default {
+  fillControlPanelData,
+  toggleLogic,
+};
